bd7bf708e829f293baa5f3466ae6530d05ac83b5
Chagall van den Berg <chagallvdb@gmail.com>
2015-03-18 17:04:14 +0000

    did two more ex's

---
+++ ex2.py
@@ -0,0 +1,9 @@
+def maxOfThree(a,b,c):
+	if a > b and a > c:
+		return a
+	elif b > c:
+		print b
+	else:
+		print c
+
+maxOfThree(8,5,9)
---
+++ ex4.py
@@ -0,0 +1,9 @@
+def isAVowel(l):
+	vowel = ['a','e','i','o','u']
+	if l in vowel:
+		return True
+	else:
+		return False 
+
+print isAVowel('k')
+print isAVowel('a')
